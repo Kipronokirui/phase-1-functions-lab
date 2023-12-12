d85049142ffd60036933ba976aed73737448925c
Kipronokirui <victorkipronokirui2@gmail.com>
2023-12-12 22:39:03 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution in this file!
+
+function distanceFromHqInBlocks(street) {
+    return Math.abs (street - 42); 
+  }
+  
+  function distanceFromHqInFeet(street) {
+    let blocks = distanceFromHqInBlocks(street);
+    return blocks * 264; }
+  
+  function distanceTravelledInFeet(start, destination) {
+    return Math.abs(destination - start) * 264; 
+  }
+  
+  function calculatesFarePrice(start, destination) {
+    let distance = distanceTravelledInFeet(start, destination);
+  
+    if (distance <= 400) {
+      return 0; 
+    } else if (distance > 400 && distance <= 2000) {
+      return (distance - 400) * 0.02; 
+    } else if (distance > 2000 && distance <= 2500) {
+      return 25; }
+      else (distance > 2500) 
+      return 'cannot travel that far'; 
+    }
+  
+  
+ 
